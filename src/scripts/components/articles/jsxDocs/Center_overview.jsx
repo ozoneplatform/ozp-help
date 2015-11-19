67b03c43b6d4040bf490f5363f165a71aba64668
bhalt <bhalteman@fearsol.com>
2015-11-19 12:37:33 -0500

    running script to sanitize html into jsx

--- src/scripts/components/articles/jsxDocs/Center_overview.jsx
+++ src/scripts/components/articles/jsxDocs/Center_overview.jsx
@@ -7,7 +7,8 @@ module.exports = React.createClass({
 	mixins: [ Router.State, Reflux.ListenerMixin],
 	contextTypes: { router: React.PropTypes.func },
 	
-render: function(){return ( 
+render: function(){
+return ( 
 <div>
 
 <h1>Understanding Center</h1>
@@ -23,7 +24,7 @@ render: function(){return (
 
 <p><img alt="Search and Discovery Page" src="Doc_images/center-search-and-discovery-page.png" title="Search and Discovery Page" /></p>
 
-<p>When you enter OZONE, the first thing you'll see is the Marketplace Search and Discovery Page. It displays Featured, New Arrivals and the Most Popular listings. Use the categories on the left to reduce the number of listings you see on the page. You can also filter by Listing Type and Organization using the drop-downs at the top of the page. Search for a listing from the search box located at the top-right of the page. Results will be returned based on listing name, description, tags, etc. You'll find a complete description of listings and search in the following related sections.</p>
+<p>When you enter OZONE, the first thing you'll see is the Marketplace Search and Discovery Page. It displays Featured, New Arrivals and the Most Popular listings. Use the categories on the left to reduce the number of listings you see on the page. You can also filter by Listing Type and Organization using the drop-downs at the top of the page. Search for a listing from the search box located at the top-right of the page. Results will be returned based on listing name, description, tags, etc. You'll find a complete description of listings and search in their corresponding help sections.</p>
 
 
 	</div>
