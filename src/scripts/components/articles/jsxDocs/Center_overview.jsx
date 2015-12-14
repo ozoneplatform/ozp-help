1dd52c7e06f3adbbcf4561bf7b79bd0f1e2d02fc
avyutzy <ashley.yutzy@thevitalteam.com>
2015-12-14 09:51:53 -0500

    Updated Center overview text

--- src/scripts/components/articles/jsxDocs/Center_overview.jsx
+++ src/scripts/components/articles/jsxDocs/Center_overview.jsx
@@ -25,7 +25,7 @@ render: function(){return (
 
 <p><img alt="Search and Discovery Page" src="Doc_images/center-search-and-discovery-page.png" title="Search and Discovery Page" /></p>
 
-<p>When you enter OZONE, the first thing you'll see is the Marketplace Search and Discovery Page. It displays Featured, New Arrivals and the Most Popular listings. Use the categories on the left to reduce the number of listings you see on the page. You can also filter by Listing Type and Organization using the drop-downs at the top of the page. Search for a listing from the search box located at the top-right of the page. Results will be returned based on listing name, description, tags, etc. You'll find a complete description of listings and search in their corresponding help sections.</p>
+<p>When you enter OZONE, the first thing you'll see is the Center, the search and discovery page. The front page displays Featured, New Arrivals and the Most Popular listings. Use the categories on the left to refine the listings displayed on the page. You can also filter by Listing Type and Organization using the drop-downs at the top of the page and search for listings with the search box located at the top-left of the page. Results will be returned based on listing name, description, tags, etc. You'll find a complete description of listings and the search tool in their corresponding help sections.</p>
 
 
 	</div>
