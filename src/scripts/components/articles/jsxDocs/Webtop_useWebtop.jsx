9fa41c8f11db1e9685202daae07823c56fb2882f
avyutzy <ashley.yutzy@thevitalteam.com>
2015-12-16 10:39:11 -0500

    updated text in Using Webtop

--- src/scripts/components/articles/jsxDocs/Webtop_useWebtop.jsx
+++ src/scripts/components/articles/jsxDocs/Webtop_useWebtop.jsx
@@ -21,7 +21,7 @@ render: function(){return (
 </iframe>
 <br /><br />
 
-<p>You access Webtop from the Global Toolbar. Click the <img alt="Webtop" src="Doc_images/Webtop_icon.png"  title="Webtop icon on the Global Toolbar" /> icon on the left side of the Global Toolbar:</p>
+<p>You access Webtop from the Toolbar. Click the <img alt="Webtop" src="Doc_images/Webtop_icon.png"  title="Webtop icon on the Global Toolbar" /> icon on the left side of the Toolbar:</p>
 
 <p><img alt="Webtop access from the toolbar" src="Doc_images/webtop-access--from-toolbar.png"  title="Webtop access from the toolbar" /><br /></p>
 
